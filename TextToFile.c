392205cd75e2ffbf4ca1c4d919af0f1bd561bb9b
yuvalar879 <yuvalar879@users.noreply.github.com>
2015-08-01 20:34:39 +0300

    Release

---
+++ TextToFile.c
@@ -0,0 +1,37 @@
+/*
+ ============================================================================
+ Name        : FileR.c
+ Author      : Yuval
+ Version     :
+ Copyright   : 
+ Description : Text to File Writer
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	FILE *fp = fopen("file.txt","w");
+
+	if (fp == NULL) {
+		puts("Error opening files");
+		exit(1);
+	}
+
+	puts("Enter text to be written to file:");
+
+	char* text = malloc(255);
+
+	gets(text);
+	fprintf(fp,"%s\n",text);
+	
+	free(text);
+
+	fclose(fp);
+	
+	
+
+	return 0;
+}
